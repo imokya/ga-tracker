83eb68504f28fdddab1ac677c0738590b5e917fb
xingway <876657294@qq.com>
2022-07-08 08:41:02 +0800

    feat: 新增声明文件

--- config/prod.config.js
+++ config/prod.config.js
@@ -1,10 +1,16 @@
 const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const baseConfig = require('./base.config.js')
+const CopyPlugin = require('copy-webpack-plugin')
 
 const config = {
   mode: 'production',
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: 'src/ga-tracker.d.ts', to: 'ga-tracker.d.ts' }]
+    })
+  ]
 }
 
 const prodConfig = merge(baseConfig, config)
---
+++ dist/ga-tracker.d.ts
@@ -0,0 +1,23 @@
+declare module ga {
+  export class GoogleAnalytics {
+    constructor()
+    public getInstance(): GoogleAnalytics
+    public getClientId(): string
+    public getScreenResolution(): strting
+    public getTracker(): Tracker
+    public getDefaultTracker(): Tracker | null
+  }
+  export class Tracker {
+    constructor(ga: GoogleAnalytics, measurementId: string)
+    public setTrackerServer(trackerServer: string): Tracker
+    public send(event: Event): Tracker
+  }
+  export class Event {
+    constructor()
+    setName(name: string): Event
+    setParams(param: any): Event
+    setCampaignParamsFromUrl(url: string): Event
+  }
+}
+
+export = ga
