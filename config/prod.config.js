0ca4ddf30e3037c436a1a9ede9d52e2216925715
xingway <876657294@qq.com>
2022-07-08 08:04:00 +0800

    new commit

--- config/prod.config.js
+++ config/prod.config.js
@@ -1,10 +1,16 @@
 const { merge } = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const baseConfig = require('./base.config.js')
+const TypescriptDeclarationPlugin = require('typescript-declaration-webpack-plugin')
 
 const config = {
   mode: 'production',
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new TypescriptDeclarationPlugin({
+      out: 'g-tracker.d.ts'
+    })
+  ]
 }
 
 const prodConfig = merge(baseConfig, config)
